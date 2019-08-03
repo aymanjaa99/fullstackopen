037b10bd5f09209bb7f3c3e20a7a1aa3ab201961
Ayman JAAOUANI <aymanjaa99@gmail.com>
2019-08-03 22:43:22 +0100

    1.2: course information, step2

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -5,18 +5,20 @@ const Header = props => {
   return <h1>{props.course}</h1>;
 };
 
+const Part = props => {
+  return (
+    <p>
+      {" "}
+      {props.part} {props.exercises}{" "}
+    </p>
+  );
+};
 const Content = props => {
   return (
     <div>
-      <p>
-        {props.parts.part1} {props.exercises.exercises1}
-      </p>
-      <p>
-        {props.parts.part2} {props.exercises.exercises2}
-      </p>
-      <p>
-        {props.parts.part3} {props.exercises.exercises3}
-      </p>
+      <Part part={props.parts.part1} exercises={props.exercises.exercises1} />
+      <Part part={props.parts.part2} exercises={props.exercises.exercises2} />
+      <Part part={props.parts.part3} exercises={props.exercises.exercises3} />
     </div>
   );
 };
