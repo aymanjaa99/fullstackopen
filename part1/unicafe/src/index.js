0e139933470dcb91bc915e5287c768f710867a96
Ayman JAAOUANI <aymanjaa99@gmail.com>
2019-08-04 12:52:08 +0100

    1.8: unicafe step3

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -4,22 +4,11 @@ import ReactDOM from "react-dom";
 const Button = ({ text, handleClick }) => {
   return <button onClick={handleClick}>{text}</button>;
 };
-const App = () => {
-  // save clicks of each button to own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
 
+const Statistics = ({ good, neutral, bad }) => {
   const all = good + bad + neutral;
   return (
     <>
-      <h1>Give feedback</h1>
-      <div>
-        <Button text="Good" handleClick={() => setGood(good + 1)} />
-        <Button text="Neutral" handleClick={() => setNeutral(neutral + 1)} />
-        <Button text="Bad" handleClick={() => setBad(bad + 1)} />
-      </div>
-      <h1>statistics</h1>
       <div>
         <p>Good: {good}</p>
         <p>Neutral: {neutral}</p>
@@ -30,6 +19,25 @@ const App = () => {
           Positive feeback: {isNaN(good / all) ? "0" : (good / all) * 100} %
         </p>
       </div>
+    </>
+  );
+};
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <>
+      <h1>Give feedback</h1>
+      <div>
+        <Button text="Good" handleClick={() => setGood(good + 1)} />
+        <Button text="Neutral" handleClick={() => setNeutral(neutral + 1)} />
+        <Button text="Bad" handleClick={() => setBad(bad + 1)} />
+      </div>
+      <h1>statistics</h1>
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </>
   );
 };
