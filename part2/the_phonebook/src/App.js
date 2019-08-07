7e679b8b59d150c31804658d826fc513b887b08d
Ayman JAAOUANI <aymanjaa99@gmail.com>
2019-08-07 21:46:18 +0100

    2.11: The Phonebook Step6

--- part2/the_phonebook/src/App.js
+++ part2/the_phonebook/src/App.js
@@ -1,17 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Filter from "./components/Filter";
 import Form from "./components/Form";
 import Persons from "./components/Persons";
-const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" }
-  ]);
 
+const App = () => {
+  const [persons, setPersons] = useState([]);
   const [filtered, setFiltered] = useState([]);
 
+  const fetch_persons = () => {
+    //console.log("Persons: ", persons);
+    axios.get("http://localhost:3001/persons").then(response => {
+      console.log("success");
+      let data = response.data;
+      setPersons(data);
+    });
+  };
+
+  useEffect(fetch_persons, []);
   return (
     <>
       <Filter persons={persons} setFiltered={setFiltered} />
